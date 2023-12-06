a4a77c9d7d24bab089abfb2460a6f983504587ed
Barış Soner Uşaklı <barisusakli@gmail.com>
2023-12-06 17:01:17 -0500

    remove login/register wrappers

--- public/components/LoginForm.js
+++ public/components/LoginForm.js
@@ -42,56 +42,54 @@ export default function LoginForm({ setUser }) {
 	}
 
 	return (
-		<div className="row justify-content-center">
-			<div className="col-12 col-md-6">
-				<div className="card shadow-sm">
-					<div className="card-body">
-						<form onSubmit={handleSubmit}>
-							<div className="mb-3">
-								<label htmlFor="email" className="form-label">Email</label>
-								<input
-									id="email"
-									className="form-control"
-									type="email"
-									placeholder="your@email.com"
-									onChange={ev => formHandleChange(ev, setFormData)}
-									name="email"
-									value={formData.email}
-									required
-									autoComplete="on"
-								/>
+		<div className="d-flex justify-content-center">
+			<div className="card shadow-sm">
+				<div className="card-body">
+					<form onSubmit={handleSubmit}>
+						<div className="mb-3">
+							<label htmlFor="email" className="form-label">Email</label>
+							<input
+								id="email"
+								className="form-control"
+								type="email"
+								placeholder="your@email.com"
+								onChange={ev => formHandleChange(ev, setFormData)}
+								name="email"
+								value={formData.email}
+								required
+								autoComplete="on"
+							/>
+						</div>
+						<div className="mb-3">
+							<div className="d-flex justify-content-between align-items-center">
+								<label htmlFor="password" className="form-label">Password</label>
+								<Link to="/reset" className="text-sm mb-2">Forgot password?</Link>
 							</div>
-							<div className="mb-3">
-								<div className="d-flex justify-content-between align-items-center">
-									<label htmlFor="password" className="form-label">Password</label>
-									<Link to="/reset" className="text-sm mb-2">Forgot password?</Link>
-								</div>
-								<input
-									id="password"
-									className="form-control"
-									type="password"
-									onChange={ev => formHandleChange(ev, setFormData)}
-									name="password"
-									value={formData.password}
-									required
-									autoComplete="on"
-								/>
-							</div>
+							<input
+								id="password"
+								className="form-control"
+								type="password"
+								onChange={ev => formHandleChange(ev, setFormData)}
+								name="password"
+								value={formData.password}
+								required
+								autoComplete="on"
+							/>
+						</div>
 
-							<div className="mb-3">
-								<HCaptcha
-									ref={captchaRef}
-									sitekey="d76f5317-946c-48e0-90aa-931c916c1d7d"
-									onVerify={(token, ekey) => handleVerificationSuccess(token, ekey)}
-    							/>
-							</div>
+						<div className="mb-3">
+							<HCaptcha
+								ref={captchaRef}
+								sitekey="d76f5317-946c-48e0-90aa-931c916c1d7d"
+								onVerify={(token, ekey) => handleVerificationSuccess(token, ekey)}
+							/>
+						</div>
 
-							<button type="submit" className="btn btn-primary fw-secondary w-100 text-center">Login</button>
-							{loginError && <p className="form-text text-danger">{loginError}</p>}
-							<hr />
-							<p className="form-text mb-0">Don't have an account? <Link to="/register">Register</Link></p>
-						</form>
-					</div>
+						<button type="submit" className="btn btn-primary fw-secondary w-100 text-center">Login</button>
+						{loginError && <p className="form-text text-danger">{loginError}</p>}
+						<hr />
+						<p className="form-text mb-0">Don't have an account? <Link to="/register">Register</Link></p>
+					</form>
 				</div>
 			</div>
 		</div>
--- public/components/RegisterForm.js
+++ public/components/RegisterForm.js
@@ -51,72 +51,70 @@ export default function RegisterForm({ setUser }) {
 	}
 
 	return (
-		<div className="row justify-content-center">
-			<div className="col-12 col-md-6">
-				<div className="card shadow-sm">
-					<div className="card-body">
-						<form onSubmit={handleSubmit}>
-							<div className="mb-3">
-								<label htmlFor="email" className="form-label">Email</label>
-								<input
-									id="email"
-									className="form-control"
-									type="email"
-									placeholder="your@email.com"
-									onChange={ev => formHandleChange(ev, setFormData)}
-									name="email"
-									value={formData.email}
-									required
-									autoComplete="off"
-								/>
-								{userNameTaken && <p className="form-text text-danger">{userNameTaken}</p>}
-							</div>
-							<div className="mb-3">
-								<label htmlFor="password" className="form-label">Password</label>
-								<input
-									id="password"
-									className="form-control"
-									type="password"
-									onChange={ev => formHandleChange(ev, setFormData)}
-									name="password"
-									value={formData.password}
-									minLength="8"
-									maxLength="64"
-									required
-									autoComplete="off"
-								/>
-							</div>
-							<div className="mb-3">
-								<label htmlFor="passwordConfirm" className="form-label">Confirm Password</label>
-								<input
-									id="passwordConfirm"
-									className="form-control"
-									type="password"
-									onChange={ev => formHandleChange(ev, setFormData)}
-									name="passwordConfirm"
-									value={formData.passwordConfirm}
-									minLength="8"
-									maxLength="64"
-									required
-									autoComplete="off"
-								/>
-								{noMatch && <p className="form-text text-danger">Passwords do no match</p>}
-							</div>
+		<div className="d-flex justify-content-center">
+			<div className="card shadow-sm">
+				<div className="card-body">
+					<form onSubmit={handleSubmit}>
+						<div className="mb-3">
+							<label htmlFor="email" className="form-label">Email</label>
+							<input
+								id="email"
+								className="form-control"
+								type="email"
+								placeholder="your@email.com"
+								onChange={ev => formHandleChange(ev, setFormData)}
+								name="email"
+								value={formData.email}
+								required
+								autoComplete="off"
+							/>
+							{userNameTaken && <p className="form-text text-danger">{userNameTaken}</p>}
+						</div>
+						<div className="mb-3">
+							<label htmlFor="password" className="form-label">Password</label>
+							<input
+								id="password"
+								className="form-control"
+								type="password"
+								onChange={ev => formHandleChange(ev, setFormData)}
+								name="password"
+								value={formData.password}
+								minLength="8"
+								maxLength="64"
+								required
+								autoComplete="off"
+							/>
+						</div>
+						<div className="mb-3">
+							<label htmlFor="passwordConfirm" className="form-label">Confirm Password</label>
+							<input
+								id="passwordConfirm"
+								className="form-control"
+								type="password"
+								onChange={ev => formHandleChange(ev, setFormData)}
+								name="passwordConfirm"
+								value={formData.passwordConfirm}
+								minLength="8"
+								maxLength="64"
+								required
+								autoComplete="off"
+							/>
+							{noMatch && <p className="form-text text-danger">Passwords do no match</p>}
+						</div>
 
-							<div className="mb-3">
-								<HCaptcha
-									ref={captchaRef}
-									sitekey="d76f5317-946c-48e0-90aa-931c916c1d7d"
-									onVerify={(token, ekey) => handleVerificationSuccess(token, ekey)}
-    							/>
-							</div>
+						<div className="mb-3">
+							<HCaptcha
+								ref={captchaRef}
+								sitekey="d76f5317-946c-48e0-90aa-931c916c1d7d"
+								onVerify={(token, ekey) => handleVerificationSuccess(token, ekey)}
+							/>
+						</div>
 
-							<button type="submit" className="btn btn-primary fw-secondary w-100 text-center">Register</button>
-							{registerError && <p className="form-text text-danger">{registerError}</p>}
-							<hr />
-							<p className="form-text mb-0">Already have an account? <Link to="/">Login</Link></p>
-						</form>
-					</div>
+						<button type="submit" className="btn btn-primary fw-secondary w-100 text-center">Register</button>
+						{registerError && <p className="form-text text-danger">{registerError}</p>}
+						<hr />
+						<p className="form-text mb-0">Already have an account? <Link to="/">Login</Link></p>
+					</form>
 				</div>
 			</div>
 		</div>
