73cebf6e88cb72ecfaa686a9eb6036646e328c8b
Barış Soner Uşaklı <barisusakli@gmail.com>
2023-12-09 10:28:09 -0500

    change landing

--- public/pages/Landing.js
+++ public/pages/Landing.js
@@ -16,8 +16,18 @@ export default function Landing({ isLoading }) {
 					<h1 className="fs-2 text-secondary fw-semibold text-center m-0">Welcome to</h1>
 					<h1 className="fw-semibold text-center m-0">Simple Budgets!</h1>
 				</div>
-				<LoginForm/>
+				<div className="d-flex flex-column justify-content-lg-center flex-lg-row flex-column-reverse gap-5">
+					<ul className="fs-5 text-secondary d-flex flex-column gap-4 my-auto mx-auto mx-lg-0">
+						<li>Quickly create budgets.</li>
+						<li>Add your transactions.</li>
+						<li>Easily track your monthly spending.</li>
+						<li>See all your spending history in one place.</li>
+						<li>100% <span className="text-decoration-underline">free</span> and mobile-friendly!</li>
+					</ul>
+					<LoginForm/>
+				</div>
 			</div>}
 		</div>
+
 	)
 }
