2cc6268de234c25b4a32d8516ed87354ef3271b2
Barış Soner Uşaklı <barisusakli@gmail.com>
2023-12-19 13:47:05 -0500

    update screenshots add them to landing

--- public/pages/Landing.js
+++ public/pages/Landing.js
@@ -11,22 +11,28 @@ export default function Landing({ isLoading }) {
 
 	return (
 		<div>
-			{ !isLoading && <div className="pt-4 d-flex flex-column gap-4">
+			{
+			!isLoading &&
+			<div className="py-4 d-flex flex-column gap-4">
 				<div>
 					<h1 className="fs-2 text-secondary fw-semibold text-center m-0">Welcome to</h1>
 					<h1 className="fw-semibold text-center m-0">Simple Budgets!</h1>
 				</div>
 				<div className="d-flex flex-column justify-content-lg-center flex-lg-row flex-column-reverse gap-5">
 					<ul className="fs-5 text-secondary d-flex flex-column gap-4 my-auto mx-auto mx-lg-0">
-						<li>Quickly create budgets.</li>
-						<li>Add your transactions.</li>
-						<li>Easily track your monthly spending.</li>
-						<li>See all your spending history in one place.</li>
+						<li>Quickly create budgets & add transactions</li>
+						<li>Track your monthly spending in one place.</li>
+						<li>Filter your spending by different budget categories.</li>
 						<li>100% <span className="text-decoration-underline">free</span> and mobile-friendly!</li>
 					</ul>
 					<LoginForm/>
 				</div>
-			</div>}
+				<hr/>
+				<h3 className="fw-semibold text-center m-0">Screenshots</h3>
+				<img src="/assets/screenshots/budgets.png" alt="budget-screen"/>
+				<img src="/assets/screenshots/transactions.png" alt="transaction-screen"/>
+			</div>
+			}
 		</div>
 
 	)
